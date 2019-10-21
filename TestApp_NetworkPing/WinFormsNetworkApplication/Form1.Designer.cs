fd285f15571a2673e0f73397f6c85f4ae5d1727e
Joseph True <jtrue41@gmail.com>
2019-10-20 20:17:53 -0400

    Add initial version of TestApp_NetworkPing

---
+++ TestApp_NetworkPing/WinFormsNetworkApplication/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPingServer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboServerName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPingServer
+            // 
+            this.btnPingServer.Location = new System.Drawing.Point(298, 35);
+            this.btnPingServer.Name = "btnPingServer";
+            this.btnPingServer.Size = new System.Drawing.Size(113, 31);
+            this.btnPingServer.TabIndex = 0;
+            this.btnPingServer.Text = "Ping";
+            this.btnPingServer.UseVisualStyleBackColor = true;
+            this.btnPingServer.Click += new System.EventHandler(this.btnPingServer_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(524, 154);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Results:";
+            // 
+            // cboServerName
+            // 
+            this.cboServerName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboServerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboServerName.FormattingEnabled = true;
+            this.cboServerName.Location = new System.Drawing.Point(15, 35);
+            this.cboServerName.Name = "cboServerName";
+            this.cboServerName.Size = new System.Drawing.Size(277, 23);
+            this.cboServerName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboServerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnPingServer);
+            this.Name = "Form1";
+            this.Text = "Network Ping Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPingServer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboServerName;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestApp_NetworkPing/WinFormsNetworkApplication/Form1.cs
@@ -0,0 +1,112 @@
+﻿// Test app to show examples of using the ping command.
+// 
+// Joseph True
+// jtrueprojects@gmail.com
+// http://www.josephtrue.com/
+// Project Repo: https://github.com/joseph-true/visual-network-explorer
+// Oct, 2019
+//
+// NOTE: Some web servers are configured to not accept ICMP commands and will not work with the ping command.
+//
+// Ping class
+// https://docs.microsoft.com/en-us/dotnet/api/system.net.networkinformation.ping?view=netframework-4.8
+
+// IPStatus Enum
+// https://docs.microsoft.com/en-us/dotnet/api/system.net.networkinformation.ipstatus?view=netframework-4.8
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Populate the combo-box list with some servers to try
+            cboServerName.Items.Add("www.google.com");
+            cboServerName.Items.Add("www.yahoo.com");
+            cboServerName.Items.Add("www.amazon.com");
+            cboServerName.Items.Add("mit.edu");
+            cboServerName.Items.Add("stanford.edu");
+            cboServerName.Items.Add("192.168.1.1");
+            // As a test, add some entries that won't work
+            cboServerName.Items.Add("---------------------");
+            cboServerName.Items.Add("not_a_valid_web_server.com");
+            cboServerName.Items.Add("192.168.1.0");
+            
+            // Display first option in list
+            cboServerName.SelectedIndex = 0;
+        }
+
+        private void btnPingServer_Click(object sender, EventArgs e)
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128, 
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted. 
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 2000;   // 2 secs
+
+            // Get name of the server to ping
+            string host = cboServerName.Text;
+
+            try
+            {
+                // Run the actual ping command
+                PingReply reply = pingSender.Send(host, timeout);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    // It worked!  Display results.
+                    string pingResults;
+                    pingResults = "Status: " + reply.Status.ToString();
+                    pingResults = pingResults + "\r\nAddress: " + reply.Address.ToString();
+                    pingResults = pingResults + "\r\nRoundTrip time: " + reply.RoundtripTime;
+                    pingResults = pingResults + "\r\nTime to live: " + reply.Options.Ttl;
+                    pingResults = pingResults + "\r\nDon't fragment: " + reply.Options.DontFragment;
+                    pingResults = pingResults + "\r\nBuffer size: " + reply.Buffer.Length;
+                    textBox1.Text = pingResults;
+                }
+                else
+                {
+                    // Something else happened. Display status.
+                    String pingMessage = "Ping command did not work:";
+                    pingMessage = pingMessage + "\r\n" + reply.Status.ToString();
+                    textBox1.Text = pingMessage;
+                }
+            }
+            catch (Exception ping_error)
+            {
+                // Some other error happened while trying to run ping.
+                String errMessage = "";
+                errMessage = ping_error.Message;
+                errMessage = errMessage + "\r\n" + ping_error.InnerException.ToString();
+                textBox1.Text = errMessage;
+
+                pingSender.Dispose();
+                return;
+            }
+        }
+    }
+}
